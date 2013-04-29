e2988d0e2c6e0b3874a6e8dc075149f05e4cd90e
Josh Holt <holt.josh@gmail.com>
2013-04-28 23:26:56 -0300

    Update app.js
    Formatting changes

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-var express 	= require('express'),
-  	routes  	= require('./routes'),
-  	http 			= require('http'),
-  	rack 			= require('asset-rack'),
+var express   = require('express'),
+  	routes    = require('./routes'),
+  	http      = require('http'),
+  	rack      = require('asset-rack'),
   	bootstrap = require('bootstrap-stylus'),
-  	path 			= require('path');
+  	path      = require('path');
 
 var assets = new rack.Rack([
   new rack.JadeAsset({
